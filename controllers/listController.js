30dc7c409eba16d097b3d449ffaf69290c27627a
adithyan s kumar <adithyans1438@gmail.com>
2024-08-01 16:05:58 +0530

    crud api for lists

--- controllers/listController.js
+++ controllers/listController.js
@@ -15,10 +15,120 @@ const create_list = async (req, res) => {
     }
 }
 
+const delete_list = async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const deletedList = await List.findByIdAndDelete(req.params.id)
+            res.status(200).json(deletedList, "Deleted")
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("You are not allowded")
+    }
+}
+const update_list = async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const updatedList = await List.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, { new: true })
+            res.status(200).json(updatedList)
+
+        } catch (error) {
+            res.status(500).json(error)
+
+        }
+
+    } else {
+        res.status(403).json("You are not allowded")
+
+    }
+}
+
+const get_list = async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = []
+    try {
+
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([
+                    {
+                        $sample: { size: 10 }
+                    },
+                    {
+                        $match: { type: typeQuery, genre: genreQuery }
+                    }
+                ])
+            } else {
+                list = await List.aggregate([
+                    {
+                        $sample: { size: 10 }
+                    },
+                    {
+                        $match: { type: typeQuery }
+                    }
+                ])
+            }
+        } else {
+            list = await List.aggregate([
+                {
+                    $sample: { size: 10 }
+                }
+            ])
+        }
+        res.status(200).json(list)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+const get_all_list = async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const lists = await List.find()
+            res.status(200).json(lists.reverse())
+
+        } catch (error) {
+            res.status(500).json(error)
+        }
+
+    } else {
+        res.status(403).json("You are not allowded")
+
+    }
+
+
+}
+
+const get_list_id = async (req, res) => {
+    if (req.user.isAdmin) {
+
+        try {
+            const list = await List.findById(req.params.id)
+            res.status(200).json(list)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("You are not allowded")
+
+    }
+}
+
+
 
 
 
 module.exports = {
-   
+    create_list,
+    delete_list,
+    get_list,
+    get_all_list,
+    get_list_id,
+    update_list
 }
 
--- routes/lists.js
+++ routes/lists.js
@@ -1,11 +1,21 @@
+const { create_list, delete_list, get_list, update_list, get_list_id, get_all_list } = require('../controllers/listController')
 const verify = require('../middleware/verifyToken')
 
 const router = require('express').Router()
 
 
 //CREATE List
-router.post('/', verify, )
-
+router.post('/', verify, create_list)
+//DELETE LIST
+router.delete('/:id', verify, delete_list)
+//Update LIST
+router.put('/:id', verify, update_list)
+//GET LIST by query
+router.get('/find', verify, get_list)
+//GET ALL LIST
+router.get('/', verify, get_all_list)
+//GET LIST by ID
+router.get('/find/:id', verify, get_list_id)
 
 
 module.exports = router
